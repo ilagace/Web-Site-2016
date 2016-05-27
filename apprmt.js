9add2452ee9a43ace5ed8446e9e69f05f16caa0c
Ivan Lagace <Ivan Lagace>
2016-05-27 14:14:41 -0400

    ALso added a new app file with proper port for the deployed version.

---
+++ apprmt.js
@@ -0,0 +1,67 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 80;
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var passport = require('passport');
+var session = require('express-session');
+
+var basenav = ['Ivan', 'Alex', 'Alex et les Lagacé', 'Alex et les Cantin', 'Vieilles Photos Lagace', 'Vieilles Photos Cantin'];
+var localbasenav = ['Yvan', 'Famille', 'Lagace', 'Cantin', 'Lagace', 'Cantin'];
+var category = ['Mountains', 'Sea', 'Cities', 'Friendship', 'Nature', 'Family', 'Culture', 'Sports'];
+var indexnav = 0;
+var pagesize = 30;
+var indexskip = '0';
+
+var navrouter = require('./src/routes/navroutes')(basenav, localbasenav, indexnav, indexskip, pagesize);
+var adminrouter = require('./src/routes/adminroutes')(basenav, localbasenav, category);
+var searchrouter = require('./src/routes/searchroutes')(basenav, localbasenav);
+var galleryrouter = require('./src/routes/galleryroutes')(basenav, localbasenav, category);
+
+app.use(express.static('public'));
+app.use('/navigation', express.static('public'));
+app.use('/gallery', express.static('public'));
+app.use('/navigation/folder', express.static('public'));
+app.use('/navigation/video', express.static('public'));
+app.use('/navigation/folder/assets', express.static('public'));
+app.use('/navigation/video/assets', express.static('public'));
+app.use('/admin/contact', express.static('public'));
+app.use('/admin/managemedia', express.static('public'));
+app.use('/admin/searchmedia', express.static('public'));
+app.set('views','./src/views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(session({secret: 'ivanlibrary'}));
+require('./src/config/passport')(app);
+
+/* how to setup handelbars
+
+var handlebars = require('express-handlebars');
+
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine','.hbs');
+
+setup ejs or jade below
+
+*/
+
+app.set('view engine','ejs');
+
+app.use('/navigation', navrouter);
+app.use('/admin', adminrouter);
+app.use('/search', searchrouter);
+app.use('/gallery', galleryrouter);
+
+app.get('/', function (req, res) {
+    res.render('index', {title:'Splash Page'});
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port ' + port);
+});
